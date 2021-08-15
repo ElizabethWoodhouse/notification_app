7c099349a4a9fce66041ab7b1b5bc8328c61c919
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-08-15 11:48:32 -0400

    feat: set-up react

---
+++ client/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import Navbar from './components/Navbar'
+// import Routes from './Routes'
+
+const App = () => {
+	return (
+        <div>
+            <p>HELLO!</p>
+			{/* <Navbar />
+      <Routes /> */}
+		</div>
+	);
+};
+
+export default App;
