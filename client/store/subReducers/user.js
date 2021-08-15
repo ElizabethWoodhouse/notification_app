7c17ba6c1dc2059ebee6ee421973d7da8df6e409
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-08-15 15:33:31 -0400

    feat: create user reducer

--- client/store/subReducers/user.js
+++ client/store/subReducers/user.js
@@ -1,23 +1,23 @@
+//updates the user state
 import axios from 'axios';
 
 const GET_USER = 'GET_USER';
 
-const getUser = (category) => ({
+const getUser = (user) => ({
 	type: GET_USER,
 	user,
 });
 
-export const fetchUser = () => async (dispatch) => {
+export const fetchUser = (username) => async (dispatch) => {
 	try {
-		//make a call to back-end to get user information
-		//username
-		// dispatch(getUser());
+		const { data: user } = await axios.get(`/api/user/${username}`);
+		dispatch(getUser(user));
 	} catch (error) {
 		console.error('Error:', error);
 	}
 };
 
-export default function category(state = {}, { type, category }) {
+export default function category(state = {}, { type, user }) {
 	switch (type) {
 		case GET_USER:
 			return user;
