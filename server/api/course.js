93eb6168cdb339232635c4830b8935874eb3b32a
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-08-15 11:43:58 -0400

    feat: create course routes

---
+++ server/api/course.js
@@ -0,0 +1,34 @@
+//file that makes calls to DB to get user's course progress
+const router = require('express').Router();
+const { CourseStatus, Course, User } = require('../db/index');
+
+// matches GET requests to /api/course/progress/:userId
+//gets courses that are in progress for specific user
+router.get('/progress/:userId', async (req, res, next) => {
+	try {
+		const uncompletedCourses = await User.findAll({
+			where: {
+				id: req.params.userId,
+			},
+			attributes: ['id', 'userName'],
+			include: [
+				{
+					model: Course,
+					attributes: ['id', 'name', 'pages'],
+					through: {
+						attributes: ['currentPage'],
+						where: { completed: false },
+						as: 'status',
+					},
+				},
+			],
+		});
+		res.status(200).send(uncompletedCourses);
+	} catch (err) {
+		next(err);
+	}
+});
+
+//future: get all completed courses for user
+
+module.exports = router;
---
+++ server/api/index.js
@@ -0,0 +1,11 @@
+//file that re-routes API requests to correct file and handles erorrs if no API route is found
+const router = require('express').Router();
+module.exports = router;
+
+router.use('/course', require('./course'));
+
+router.use((req, res, next) => {
+	const error = new Error('API route requested not found');
+	error.status = 404;
+	next(error);
+});
